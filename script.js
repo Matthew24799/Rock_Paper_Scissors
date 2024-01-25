5573e8fd04542b8bca5b06e4677352bef8c51ceb
Matthew24799 <157596330+Matthew24799@users.noreply.github.com>
2024-01-24 19:38:57 -0500

    Added comments
    Added comments throughout file

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-let wins = 0;
-let loss = 0;
+let wins = 0; // amount of wins the player has 
+let loss = 0; // amount of losses the player has 
 
-function getComputerChoice() {
-    let computerChoices = ["rock", "paper", "scissors"];
-   let random = Math.floor(Math.random() * computerChoices.length);
-   let computerChoice = computerChoices[random];
-   console.log("Computer picked: " + computerChoice);
-   return computerChoice;
+function getComputerChoice() {                                                    // a function to create an array and then pick one of the three choices from it 
+    let computerChoices = ["rock", "paper", "scissors"];                           // creates array
+   let random = Math.floor(Math.random() * computerChoices.length);                // randomly assigns random to a number between 1-3
+   let computerChoice = computerChoices[random];                                   // assigns computerChoice to one of the arrays items depending on the value of the variable above
+   console.log("Computer picked: " + computerChoice);                              // logs it 
+   return computerChoice;                                                          // returns the value 
 
 
 }
 
 
-function playRound(playerChoice, computerChoice) {
+function playRound(playerChoice, computerChoice) {                                // function to play a single round with the game rule set built in and also keeps counts of wins and losses
     if (playerChoice == computerChoice) {
         return "IT'S A TIE";
     } else if ( (playerChoice == "rock" && computerChoice == "scissors") ||
@@ -28,22 +28,22 @@ function playRound(playerChoice, computerChoice) {
 }
 
 
-while (wins < 3 && loss < 3) {
-    let playerChoice = prompt("Enter Rock, Paper, or Scissors");
-playerChoice = playerChoice.toLocaleLowerCase();
-console.log("Player picked: " + playerChoice);
-let computerChoice = getComputerChoice();
+while (wins < 3 && loss < 3) {                                                      // a loop for the game to play a best of 3
+    let playerChoice = prompt("Enter Rock, Paper, or Scissors");                       // ask player for an input
+playerChoice = playerChoice.toLocaleLowerCase();                                        // makes it lowercase so theres no mistake
+console.log("Player picked: " + playerChoice);                                          // logs it
+let computerChoice = getComputerChoice();                                                  // makes a variable to store the computerChoice from the function
 
 
-console.log(playRound(playerChoice, computerChoice));
-console.log("Player Score: " + wins);
-console.log("Computer Score: " + loss);
+console.log(playRound(playerChoice, computerChoice));                                       // calls the playRound function with the parameters 
+console.log("Player Score: " + wins);                                                       // logs win
+console.log("Computer Score: " + loss);                                                     // logs losses
 
 
 }
 
-if (wins == 3) {
+if (wins == 3) {                                                                // states the winner 
     console.log("You Won The Game");
 } else if (loss == 3) {
-    console.log("You Lost The Game");
+    console.log("You Lost The Game");                                           // states the loser 
 }
